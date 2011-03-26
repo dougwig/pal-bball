f8f2aeebdd08b54077c0568a9f735db0e545b869
Doug Wiegley <doug@ifanmedia.com>
2011-03-25 23:15:25 -0600

    Tweak scaffold skeleton

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,2 +1,23 @@
 class Signup < ActiveRecord::Base
+  validates_presence_of :team_sex
+  validates_presence_of :coach_name, :coach_email, :coach_phone
+  validates_presence_of :player1_name, :player1_dob_month, :player1_dob_day, :player1_dob_year
+  validates_presence_of :player2_name, :player2_dob_month, :player2_dob_day, :player2_dob_year
+  validates_presence_of :player3_name, :player3_dob_month, :player3_dob_day, :player3_dob_year
+  validates_presence_of :player4_name, :player4_dob_month, :player4_dob_day, :player4_dob_year
+  
+  def TEAM_SEXES
+    [ "Boys", "Girls" ]
+  end
+  
+  def DOB_MONTHS
+    [nil] + (1..12).to_a
+  end
+  def DOB_DAYS
+    [nil] + (1..31).to_a
+  end
+  def DOB_YEARS
+    [nil] + (Time.now.year-20..Time.now.year).to_a
+  end
+  
 end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110326045404) do
+
+  create_table "signups", :force => true do |t|
+    t.string   "team_sex"
+    t.string   "coach_name"
+    t.string   "coach_phone"
+    t.string   "coach_address"
+    t.string   "coach_email"
+    t.string   "player1_name"
+    t.string   "player1_address"
+    t.string   "player1_phone"
+    t.string   "player1_email"
+    t.integer  "player1_dob_month"
+    t.integer  "player1_dob_day"
+    t.integer  "player1_dob_year"
+    t.string   "player2_name"
+    t.string   "player2_address"
+    t.string   "player2_phone"
+    t.string   "player2_email"
+    t.integer  "player2_dob_month"
+    t.integer  "player2_dob_day"
+    t.integer  "player2_dob_year"
+    t.string   "player3_name"
+    t.string   "player3_address"
+    t.string   "player3_phone"
+    t.string   "player3_email"
+    t.integer  "player3_dob_month"
+    t.integer  "player3_dob_day"
+    t.integer  "player3_dob_year"
+    t.string   "player4_name"
+    t.string   "player4_address"
+    t.string   "player4_phone"
+    t.string   "player4_email"
+    t.integer  "player4_dob_month"
+    t.integer  "player4_dob_day"
+    t.integer  "player4_dob_year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
